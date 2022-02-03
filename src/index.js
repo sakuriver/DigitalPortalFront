032ffa1fef89b9c06c5ee5b70c84e136792ce04f
sakuriver <suzuerain@yahoo.co.jp>
2022-02-04 03:23:41 +0900

    不要物を統合

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ReactDOM from 'react-dom'
 
 function tick() {
     ReactDOM.render(<App />, document.getElementById('root'));
