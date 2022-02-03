eefc9e0eb7e2990d934883a0a7fc5489cfaffe43
sakuriver <suzuerain@yahoo.co.jp>
2022-02-04 03:21:12 +0900

    エラー対応

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactDOM from 'react-dom'
 
 function tick() {
     ReactDOM.render(<App />, document.getElementById('root'));
