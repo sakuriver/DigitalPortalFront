6b534308c5f5476b2c703ddd6864c186f8678e72
sakuriver <suzuerain@yahoo.co.jp>
2022-02-04 02:44:35 +0900

    時計機能テスト

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function App() {
   const value = 'DigitalPortal';
-  return <div>Hello {value}</div>;
+  const element = (
+    <div>
+       <h1>Hello {value}</h1>
+       <h2>It is {new Date().toLocaleDateTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
 }
 
 export default App;
