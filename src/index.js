bc2693c4eacc401ad5ab5c8afec6738bb813473a
sakuriver <suzuerain@yahoo.co.jp>
2022-02-04 03:38:40 +0900

    test

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-function tick() {
-    ReactDOM.render(<App />, document.getElementById('root'));
-}
-
-
-setInterval(tick, 1000);
+ReactDOM.render(<App />, document.getElementById('root'));
