8052c3d77f8ac3ffd5c7c6f51d52813ec3bfb698
sakuriver <suzuerain@yahoo.co.jp>
2022-02-04 03:28:57 +0900

    テスト

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'DigitalPortal';
-  const element = (
+  return
     <div>
        <h1>Hello {value}</h1>
        <h2>It is {new Date().toLocaleDateTimeString()}</h2>
     </div>
-  );
-  return element;
 }
 
 export default App;
